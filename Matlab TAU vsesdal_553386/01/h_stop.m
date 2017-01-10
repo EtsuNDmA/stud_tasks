ba1a38884d5679eb13cb45641a2d6775667b150e
Dmitry Nikolaev <etsu.nikolaev@gmail.com>
2017-01-10 18:38:59 +0300

    Start new matlab tau task

---
+++ Matlab TAU vsesdal_553386/01/h_stop.m
@@ -0,0 +1,31 @@
+%Построение графиков элементов матричной переходной функции (файл h_stop.m)
+%
+close all
+figure
+plot(t_,h11_s,'r-',t_,h11_vm,'b--',t_,h11_a,'m-.')
+grid on
+xlabel('t, c')
+ylabel('h11')
+title('Графики элемента h11 матричной переходной функции')
+legend('h11-struct','h11-VM','h11-analit',0)
+figure
+plot(t_,h21_s,'r-',t_,h21_vm,'b--',t_,h21_a,'m-.')
+grid on
+xlabel('t, c')
+ylabel('h21')
+title('Графики элемента h21 матричной переходной функции')
+legend('h21-struct','h21-VM','h21-analit',0)
+figure
+plot(t_,h12_s,'r-',t_,h12_vm,'b--',t_,h12_a,'m-.')
+grid on
+xlabel('t, c')
+ylabel('h12')
+title('Графики элемента h12 матричной переходной функции')
+legend('h12-struct','h12-VM','h12-analit',0)
+figure
+plot(t_,h22_s,'r-',t_,h22_vm,'b--',t_,h22_a,'m-.')
+grid on
+xlabel('t, c')
+ylabel('h22')
+title('Графики элемента h22 матричной переходной функции')
+legend('h22-struct','h22-VM','h22-analit',0)
---
+++ Matlab TAU vsesdal_553386/01/w_stop.m
@@ -0,0 +1,31 @@
+%Построение графиков элементов матричной весовой функции (файл w_stop.m)
+%
+close all
+figure
+plot(t_,w11_s,'r-',t_,w11_vm,'b--',t_,w11_a,'m-.')
+grid on
+xlabel('t, c')
+ylabel('w11')
+title('Графики элемента w11 матричной весовой функции')
+legend('w11-struct','w11-VM','w11-analit',0)
+figure
+plot(t_,w21_s,'r-',t_,w21_vm,'b--',t_,w21_a,'m-.')
+grid on
+xlabel('t, c')
+ylabel('w21')
+title('Графики элемента w21 матричной весовой функции')
+legend('w21-struct','w21-VM','w21-analit',0)
+figure
+plot(t_,w12_s,'r-',t_,w12_vm,'b--',t_,w12_a,'m-.')
+grid on
+xlabel('t, c')
+ylabel('w12')
+title('Графики элемента w12 матричной весовой функции')
+legend('w12-struct','w12-VM','w12-analit',0)
+figure
+plot(t_,w22_s,'r-',t_,w22_vm,'b--',t_,w22_a,'m-.')
+grid on
+xlabel('t, c')
+ylabel('w22')
+title('Графики элемента w22 матричной весовой функции')
+legend('w22-struct','w22-VM','w22-analit',0)
