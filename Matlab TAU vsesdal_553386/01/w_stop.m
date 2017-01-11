172af6ef9c1f986caf91a95832382a093a3d5482
EtsuNDmA <etsu.nikolaev@gmail.com>
2017-01-12 00:03:33 +0300

    1 задание готово

--- Matlab TAU vsesdal_553386/01/w_stop.m
+++ Matlab TAU vsesdal_553386/01/w_stop.m
@@ -1,31 +1,36 @@
-%РџРѕСЃС‚СЂРѕРµРЅРёРµ РіСЂР°С„РёРєРѕРІ СЌР»РµРјРµРЅС‚РѕРІ РјР°С‚СЂРёС‡РЅРѕР№ РІРµСЃРѕРІРѕР№ С„СѓРЅРєС†РёРё (С„Р°Р№Р» w_stop.m)
-%
-close all
-figure
-plot(t_,w11_s,'r-',t_,w11_vm,'b--',t_,w11_a,'m-.')
-grid on
-xlabel('t, c')
-ylabel('w11')
-title('Р“СЂР°С„РёРєРё СЌР»РµРјРµРЅС‚Р° w11 РјР°С‚СЂРёС‡РЅРѕР№ РІРµСЃРѕРІРѕР№ С„СѓРЅРєС†РёРё')
-legend('w11-struct','w11-VM','w11-analit',0)
-figure
-plot(t_,w21_s,'r-',t_,w21_vm,'b--',t_,w21_a,'m-.')
-grid on
-xlabel('t, c')
-ylabel('w21')
-title('Р“СЂР°С„РёРєРё СЌР»РµРјРµРЅС‚Р° w21 РјР°С‚СЂРёС‡РЅРѕР№ РІРµСЃРѕРІРѕР№ С„СѓРЅРєС†РёРё')
-legend('w21-struct','w21-VM','w21-analit',0)
-figure
-plot(t_,w12_s,'r-',t_,w12_vm,'b--',t_,w12_a,'m-.')
-grid on
-xlabel('t, c')
-ylabel('w12')
-title('Р“СЂР°С„РёРєРё СЌР»РµРјРµРЅС‚Р° w12 РјР°С‚СЂРёС‡РЅРѕР№ РІРµСЃРѕРІРѕР№ С„СѓРЅРєС†РёРё')
-legend('w12-struct','w12-VM','w12-analit',0)
-figure
-plot(t_,w22_s,'r-',t_,w22_vm,'b--',t_,w22_a,'m-.')
-grid on
-xlabel('t, c')
-ylabel('w22')
-title('Р“СЂР°С„РёРєРё СЌР»РµРјРµРЅС‚Р° w22 РјР°С‚СЂРёС‡РЅРѕР№ РІРµСЃРѕРІРѕР№ С„СѓРЅРєС†РёРё')
-legend('w22-struct','w22-VM','w22-analit',0)
+%Построение графиков элементов матричной весовой функции
+close all 
+ 
+figure 
+plot(t_,w11_s,'r-',t_,w11_vm,'b--',t_,w11_a,'m-.') 
+grid on 
+xlabel('t, c') 
+ylabel('w11') 
+title('Графики элемента w11 матричной весовой функции') 
+legend('w11-struct','w11-VM','w11-analit',0) 
+ 
+figure 
+plot(t_,w21_s,'r-',t_,w21_vm,'b--',t_,w21_a,'m-.') 
+grid on 
+xlabel('t, c') 
+ylabel('w21') 
+title('Графики элемента w21 матричной весовой функции') 
+legend('w21-struct','w21-VM','w21-analit',0) 
+ 
+figure 
+plot(t_,w12_s,'r-',t_,w12_vm,'b--',t_,w12_a,'m-.') 
+grid on 
+xlabel('t, c') 
+ylabel('w12') 
+title('Графики элемента w12 матричной весовой функции') 
+legend('w12-struct','w12-VM','w12-analit',0) 
+ 
+figure 
+plot(t_,w22_s,'r-',t_,w22_vm,'b--',t_,w22_a,'m-.') 
+grid on 
+xlabel('t, c') 
+ylabel('w22') 
+title('Графики элемента w22 матричной весовой функции') 
+legend('w22-struct','w22-VM','w22-analit',0) 
+ 
+
