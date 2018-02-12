f436a352dac1ef9d3df4bd82afb0f97724a85c71
Dmitry Nikolaev <etsu.nikolaev@gmail.com>
2018-02-12 13:16:20 +0300

    изменения по просьбе заказчика

--- Python_fuzzy_decision_tree_vsesdal_867552/fid3.py
+++ Python_fuzzy_decision_tree_vsesdal_867552/fid3.py
@@ -3,6 +3,8 @@ import numpy as np
 from collections import deque
 from itertools import count
 from copy import deepcopy
+import skfuzzy as fuzz
+import json
 
 
 def pprint(tree, level=0, file=None):
@@ -32,7 +34,7 @@ class Node(object):
         if self.children:
             return "{}-->N{}-{}".format(self.f_attribute or "Root", self.id, self.attribute)
         else:
-            return "{}-->N{}-C({:.2f}, {:.2f})".format(self.f_attribute, self.id,  self.C[0], self.C[1])
+            return "{}-->N{}-C({})".format(self.f_attribute, self.id,  self.C)
 
     def add_child(self, node):
         """Добавляет дочерний узел"""
@@ -62,7 +64,7 @@ class FuzzyID3Classifier:
         self.tree = None
         self.classes = None
         self._attribute_columns = None
-        
+
         self._train_len = None
         self._fitted = False
 
@@ -78,8 +80,8 @@ class FuzzyID3Classifier:
     def _fuzzy_entropy(self, X, y, attribute):
         """Вычисляет нечеткую энтропию для каждого столбца из X по классам из y"""
         col = self.fuzzy_attributes[attribute]['column']
-        m_funcs = self.fuzzy_attributes[attribute]['functions']
-        MF = np.hstack([func(X[:, col].reshape((X.shape[0], 1))) for func in m_funcs])
+        f_params = self.fuzzy_attributes[attribute]['params']
+        MF = np.hstack([fuzzy_func(X[:, col], params).reshape(X.shape[0], 1) for params in f_params])
         S = np.sum(MF, axis=0)
 
         Sc = np.zeros((len(self.classes), MF.shape[1]))
@@ -128,10 +130,10 @@ class FuzzyID3Classifier:
         node.attribute = attribute
 
         col = self._attribute_columns[attribute]
-        m_funcs = self.fuzzy_attributes[attribute]['functions']
+        f_params = self.fuzzy_attributes[attribute]['params']
         names = self.fuzzy_attributes[attribute]['names']
-        M = np.hstack([func(node.X[:, col].reshape((node.X.shape[0], 1))) for func in m_funcs])
-        for m_func, f_attribute, M_col in zip(m_funcs, names, M.T):
+        M = np.hstack([fuzzy_func(node.X[:, col], params).reshape(node.X.shape[0], 1) for params in f_params])
+        for f_attribute, M_col in zip(names, M.T):
             X = node.X[M_col > 0, :]
             y = node.y[M_col > 0]
             node.add_child(Node(X, y, f_attribute=f_attribute, classes=self.classes))
@@ -173,7 +175,7 @@ class FuzzyID3Classifier:
     def score(self, y_pred, y):
         """Возвращает метрики качества precision и recall"""
         CM = self.confusion_matrix(y_pred, y)
-        
+
         precision = np.zeros(CM.shape[0])
         recall = np.zeros(CM.shape[0])
         for i in range(CM.shape[0]):
@@ -200,8 +202,8 @@ class FuzzyID3Classifier:
                 attribute = cur_node.attribute
                 col = self.fuzzy_attributes[attribute]['column']
                 for i, child in enumerate(cur_node.children):
-                    m_func = self.fuzzy_attributes[attribute]['functions'][i]
-                    child.m = cur_node.m * m_func(X[col])
+                    f_params = self.fuzzy_attributes[attribute]['params'][i]
+                    child.m = cur_node.m * fuzzy_func(X[col], f_params)
                     stack.append(child)
             else:
                 leafs.append(cur_node)
@@ -217,115 +219,47 @@ def train_test_split(X, y, test_size=0.33):
     train_indexes = np.array(tuple(set(indexes) - set(test_indexes)))
     return X[train_indexes, :], X[test_indexes, :], y[train_indexes], y[test_indexes]
 
+def fuzzy_func(x, params):
+    x = np.array(x)
+    if x.shape == ():
+        x = x.reshape(1)
+    params = np.array(params)
+    if params.size == 2 and params[0] <= params[1]:
+        return fuzz.smf(x, *params)
+    elif params.size == 2 and params[0] > params[1]:
+        return fuzz.zmf(x, *params[::-1])
+    elif params.size == 3:
+        assert params[0] <= params[1] <= params[2]
+        return fuzz.trimf(x, params)
+    elif params.size == 4:
+        assert params[0] <= params[1] <= params[2] <= params[3]
+        return fuzz.trapmf(x, params)
+    else:
+        raise ValueError('Wrong number of params')
+
 
 if __name__ == '__main__':
-    
+    # Загрузим параметры нечетких функций
+    with open('params.json', 'r') as fp:
+      params = json.load(fp)
+
+    classes = params['classes']
+    fuzzy_attributes = params['attributes']
     # Загрузим датасет, исключив 1 строку
     dataset = np.loadtxt('input.csv', delimiter=",", skiprows=1)
     X = dataset[:, :-1]
     y = dataset[:, -1].astype(int)
-    
-    # Создадим функции для вычисления нечтких аргументов
-    def t_cool(x):
-        if x < 15:
-            return 1.0
-        else:
-            return 0.0
-
-    def t_mild(x):
-        if x < 5:
-            return 0.0
-        elif x < 20:
-            return x/15 - 1/3
-        elif x < 30:
-            return 1.0
-        elif x < 35:
-            return -x/5 + 7
-        else:
-            return 0.0
-
-    def t_hot(x):
-        if x < 25:
-            return 0.0
-        elif x <= 35:
-            return x/10 - 2.5
-        else:
-            return 1.0
-
-    def w_weak(x):
-        if x < 3:
-            return 1.0
-        elif x <= 5:
-            return 2.5 - x/2
-        else:
-            return 0.0
-
-    def w_strong(x):
-        if x < 3:
-            return 0.0
-        elif x <= 8:
-            return x/5 - 0.6
-        else:
-            return 1.0
-
-    def tj_short(x):
-        if x < 3:
-            return 1.0
-        elif x <= 9:
-            return 1.5 - x/6
-        else:
-            return 0.0
-
-    def tj_long(x):
-        if x < 5:
-            return 0.0
-        elif x <= 15:
-            return x/10 - 0.5
-        else:
-            return 1.0
-    
-    # Векторизуем функции, чтобы они могли работать сразу с массивами
-    # в качестве входных аргументов
-    t_hot = np.vectorize(t_hot)
-    t_mild = np.vectorize(t_mild)
-    t_cool = np.vectorize(t_cool)
-
-    w_weak = np.vectorize(w_weak)
-    w_strong = np.vectorize(w_strong)
-
-    tj_short = np.vectorize(tj_short)
-    tj_long = np.vectorize(tj_long)
-    
-    fuzzy_attributes = {
-        'temperature': {
-            'functions': [t_hot, t_mild, t_cool],
-            'names': ['hot', 'mild', 'cool'],
-            'column': 0,
-        },                      
-        'wind': {
-            'functions': [w_weak, w_strong],
-            'names': ['weak', 'strong'],
-            'column': 1,
-        },
-        'traffic': {
-            'functions': [tj_long, tj_short],
-            'names': ['tj_long', 'tj_short'],
-            'column': 2,
-        },
-    }
-    classes = [0, 1]
-    
     # Разделим датасет
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
-    
+
     # Создадим и обучим классификатор
     clf = FuzzyID3Classifier(control_threshold=0.85, decision_threshold=0.02)
     clf.fit(X_train, y_train, fuzzy_attributes, classes)
-    
+
     # Распечатаем дерево в файл
     with open('out_tree.txt', 'w') as f:
         pprint(clf.tree, file=f)
-    
+
     # Предсказанные классов
     c_memb = clf.predict(X_test)
     # Предсказанные классы
